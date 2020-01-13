c7079488efd4138fc470642b0a19b882b7f712cc
Bruno <renato.romao@go-it.work>
2020-01-13 13:30:30 -0300

    v1.0.3-5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-export * from './utils/stringHelpers';
+const specialChar = /[\s`~!@#~´º%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+const whiteSpace = / /g;
+const uniCAccents = /[\u0300-\u036f]/g;
+
+export function capitalizer(text: string) {
+  return text.replace(/(?:^|\s)\S/g, function(a: string) {
+    return a.toUpperCase();
+  });
+}
+
+export function strFormatter(str: string) {
+  return capitalizer(str)
+    .replace(whiteSpace, '')
+    .replace(specialChar, '')
+    .normalize('NFD')
+    .replace(uniCAccents, '');
+}
+
+export const objFormatter = (obj: object) => {
+  if (
+    obj === null ||
+    (Object.getPrototypeOf(obj).constructor.name !== 'Array' &&
+      Object.getPrototypeOf(obj).constructor.name !== 'Object')
+  )
+    return obj;
+  return Object.keys(obj).reduce(
+    (acc, key) => {
+      //@ts-ignore
+      acc[strFormatter(key)] = typeof obj[key] == 'string' ? obj[key].trim() : objFormatter(obj[key]);
+      return acc;
+    },
+    Array.isArray(obj) ? [] : {},
+  );
+};
