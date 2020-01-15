d299582dd0cb97288c18779eab253030d8547f7c
Bruno <renato.romao@go-it.work>
2020-01-15 10:42:27 -0300

    v1.0.4-6

--- src/__tests__/stringFn.test.ts
+++ src/__tests__/stringFn.test.ts
@@ -2,23 +2,24 @@ import { capitalizer, strFormatter, objFormatter } from '../utils/stringHelpers'
 
 describe('String functions helpers tests', () => {
   it('Should capititalize any string', () => {
-    expect(capitalizer('bruno')).toBe('Bruno');
+    expect(capitalizer('bruno kot')).toBe('Bruno Kot');
   });
   //Strings naming convertions tests:
   it('Should apply capitalizer(), then remove whitespaces, remove special chars and remove accents', () => {
-    expect(strFormatter(' brãI Né')).toBe('BraINe');
+    //Erro segunda coluna manten uma letra maiscula se existir após o começo
+    expect(strFormatter(' bãd Ob*jéct%;')).toBe('BadObject');
   });
 
   it('Should format to camelCase', () => {
-    expect(strFormatter(' brãI Né', 'camelCase')).toBe('braINe');
+    expect(strFormatter(' bãd Ob*ject%;', 'camelCase')).toBe('badObject');
   });
 
   it('Should format to underscore', () => {
-    expect(strFormatter(' brãI Né', 'underscore')).toBe('brai_ne');
+    expect(strFormatter('  bãd Ob*ject%;', 'underscore')).toBe('bad_object');
   });
 
   it('Should format to lowercase', () => {
-    expect(strFormatter(' brãI N  é     ', 'lowercase')).toBe('braine');
+    expect(strFormatter(' bãd Ob*jec t%;', 'lowercase')).toBe('badobject');
   });
 });
 
